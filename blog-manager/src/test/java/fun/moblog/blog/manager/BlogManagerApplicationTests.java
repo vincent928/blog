eafb04d97f6feef6ef6612286737c07529346ff0
caoxm <caoxm@citydo.com.cn>
2019-11-14 15:38:43 +0800

    1.项目初始化 新增web与manager模块

---
+++ blog-manager/src/test/java/fun/moblog/blog/manager/BlogManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.moblog.blog.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
