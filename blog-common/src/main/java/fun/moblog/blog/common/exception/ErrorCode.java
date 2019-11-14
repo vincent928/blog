25c9486ddfdd9db6bc4ef0d8ec08fde2abdbf823
caoxm <caoxm@citydo.com.cn>
2019-11-14 17:32:40 +0800

    1.项目初始化

---
+++ blog-common/src/main/java/fun/moblog/blog/common/exception/ErrorCode.java
@@ -0,0 +1,14 @@
+package fun.moblog.blog.common.exception;
+
+/**
+ * @Author : moon
+ * @Date : 2019/11/14 15:44
+ * @Description :
+ */
+public interface ErrorCode {
+
+    int getCode();
+
+    String getMsg();
+
+}
